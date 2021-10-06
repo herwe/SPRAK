cf36f48e8e667bef2656aae9e0f7874ee90aeb8c
FredrikEngstromGit <tNdxTvZ76ANuJNi>
2021-10-06 11:31:42 +0200

    Print menu options

--- src/main/java/DialogMenu.java
+++ src/main/java/DialogMenu.java
@@ -1,5 +1,11 @@
 public class DialogMenu {
     public void menuLoop(){
-
+        while (true){
+            System.out.println("* * * H Ä N G A G U B B E * * *" + "\n" +
+                    "1. Spela" + "\n" +
+                    "2. Hur spelar man?" + "\n" +
+                    "3. Lägg till ord" + "\n" +
+                    "0. Avsluta");
+        }
     }
 }
